41d0cbbcd3d9c1e55dcc77c03746e8f5a34bca8a
Nigel Kukard <nkukard@lbsd.net>
2007-06-08 21:40:35 +0000

    * Added time to greylisting

--- cbp/greylisting.pm
+++ cbp/greylisting.pm
@@ -24,9 +24,11 @@ sub check {
 	# We only valid in the RCPT state
 	return 0 if (!defined($request->{'protocol_state'}) || $request->{'protocol_state'} ne "RCPT");
 
+	my $time = time();
+
 	# Log what we see
 	logModule(2,
-		sprintf('Server: %s, From: %s, To: %s'."\n",$request->{'client_address'},$request->{'sender'},$request->{'recipient'})
+		sprintf('Server: %s, From: %s, To: %s, Time: %s'."\n",$request->{'client_address'},$request->{'sender'},$request->{'recipient'},$time);
 	);	
 #setCheckResult("action=DEFER_IF_PERMIT Policy Rejection: Greylisted");
 
