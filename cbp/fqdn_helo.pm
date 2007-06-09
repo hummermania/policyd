e270231657925bcde02fa0ef050bf5f1562936e9
Nigel Kukard <nkukard@lbsd.net>
2007-06-09 00:06:28 +0000

    * Added DNS checking of HELO/EHLO

--- cbp/fqdn_helo.pm
+++ cbp/fqdn_helo.pm
@@ -5,6 +5,7 @@
 
 package cbp::fqdn_helo;
 
+use Net::DNS;
 use cbp::modules;
 
 
@@ -31,19 +32,39 @@ sub check {
 	# FIXME - configureation, bypass FQDN checks for SASL users
 	return 0 if (defined($request->{'sasl_username'}) && $request->{'sasl_username'} ne "");
 
-	# FIXME - configuration, allow address literal
+	# FIXME - configuration, restrict to address literal
 	# Check if helo is an address literal
 	return 0 if ($request->{'helo_name'} =~ /^\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\]$/);
 
-	# FIXME - configuration, allow FQDN
+	# FIXME - configuration, restrict to FQDN
 	# Check if helo is a FQDN - Only valid characters in a domain is alnum and a -
-	return 0 if ($request->{'helo_name'} =~ /^[\w-]+(\.[\w-]+)+$/);	
+	if ($request->{'helo_name'} =~ /^[\w-]+(\.[\w-]+)+$/) {
+		# FIXME - configuration, resolvable FQDN
+		my $res = Net::DNS::Resolver->new;
+		my $query = $res->search($request->{'helo_name'});
+		# If the query failed
+		if (!$query) {
+			# Check errror
+			if ($res->errorstring eq "NXDOMAIN") {
+				logModule(2,"Rejecting HELO/EHLO '".$request->{'helo_name'}."', not found.");
+				setCheckResult("action=REJECT Invalid HELO/EHLO: Does not resolve");
+				return 1;
+			if ($res->errorstring eq "SERVFAIL") {
+				setCheckResult("action=DEFER_IF_PERMIT Cannot resolve HELO/EHLO");
+				return 1;  # FIXME - Use proper defer-if-permit here and return 0?
+			} else {
+				logModule(2,"Unknown error resolving '".$request->{'helo_name'}."': ".$res->errorstring);
+			}
+         }
+
+		 return 0;
+	}
 
 
 	logModule(2,"Rejecting HELO/EHLO '".$request->{'helo_name'}."'");
 
 
-	setCheckResult("action=REJECT RFC2821 requires FQDN HELO/EHLO");
+	setCheckResult("action=REJECT Invalid HELO/EHLO: RFC2821 requires FQDN HELO/EHLO");
 
 	return 1;
 }
