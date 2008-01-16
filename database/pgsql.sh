e03075e03bc69700a9ad67d258722d804cb27bb3
Nigel Kukard <nkukard@lbsd.net>
2008-01-16 11:15:13 +0000

    * Added script to convert tsql files into mysql, pgsql and sqlite format

--- database/pgsql.sh
+++
@@ -1,7 +0,0 @@
-sed \
-	-e 's/@PRELOAD@/SET CONSTRAINTS ALL DEFERRED;/' \
-	-e 's/@POSTLOAD@//' \
-	-e 's/@CREATE_TABLE_SUFFIX@//' \
-	-e 's/@SERIAL_TYPE@/SERIAL/' \
-	-e 's/@BIG_INTEGER@/INT8/' \
-	-e 's/@SERIAL_REF_TYPE@/INT8/'
--- database/sqlite3.sh
+++
@@ -1,7 +0,0 @@
-sed \
-	-e 's/@PRELOAD@//' \
-	-e 's/@POSTLOAD@//' \
-	-e 's/@CREATE_TABLE_SUFFIX@//' \
-	-e 's/@SERIAL_TYPE@/INTEGER PRIMARY KEY AUTOINCREMENT/' \
-	-e 's/@BIG_INTEGER@/INT8/' \
-	-e 's/@SERIAL_REF_TYPE@/INT8/'
