28c7ea7e7404734d7a2c0f7c4be35fa0b724ecde
Nigel Kukard <nkukard@lbsd.net>
2008-03-16 09:42:19 +0000

    * Added some debugging info to tracking.pm to try catch uninitialized data bug

--- cbp/tracking.pm
+++ cbp/tracking.pm
@@ -290,6 +290,8 @@ sub updateSessionData
 		# Get encoded policy data
 		my $policyData = encodePolicyData($sessionData->{'Recipient'},$sessionData->{'Policy'});
 		# Generate recipient data
+		$server->log(LOG_ERR,"[TRACKING] RecipientDATA is NULL") if (!defined($sessionData->{'RecipientData'}));
+		$server->log(LOG_ERR,"[TRACKING] PolicyDATA is NULL") if (!defined($policyData));
 		my $recipientData = $sessionData->{'RecipientData'}."/$policyData";
 
 		$server->log(LOG_DEBUG,"[TRACKING] RecipientData = $recipientData");
