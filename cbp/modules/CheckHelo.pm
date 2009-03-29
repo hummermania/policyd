f01eb52b5657a86be4d69dcb90f475ab446b0d73
Nigel Kukard <nkukard@lbsd.net>
2009-03-29 12:48:09 +0000

    * Fixed spurious "REJECT" in CheckHelo module

--- cbp/modules/CheckHelo.pm
+++ cbp/modules/CheckHelo.pm
@@ -377,7 +377,7 @@ sub check {
 					$sessionData->{'Sender'},
 					$sessionData->{'Recipient'});
 
-			return $server->protocol_response(PROTO_REJECT,"REJECT","Invalid HELO/EHLO; Blacklisted");
+			return $server->protocol_response(PROTO_REJECT,"Invalid HELO/EHLO; Blacklisted");
 		}
 	}
 
