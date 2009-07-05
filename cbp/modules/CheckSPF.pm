abf18774c7b54efe0d4c60d840d28a27f97a0a53
Nigel Kukard <nkukard@lbsd.net>
2009-07-05 14:53:45 +0000

    * Fixed indentation issues and trailing whitespaces on some lines

--- cbp/modules/CheckSPF.pm
+++ cbp/modules/CheckSPF.pm
@@ -1,17 +1,17 @@
 # SPF checking module
 # Copyright (C) 2009, AllWorldIT
 # Copyright (C) 2008, LinuxRulz
-# 
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
@@ -90,7 +90,7 @@ sub check {
 
 	# Policy we're about to build
 	my %policy;
-	
+
 	# Loop with priorities, high to low
 	foreach my $priority (sort {$a <=> $b} keys %{$sessionData->{'Policy'}}) {
 
@@ -141,17 +141,17 @@ sub check {
 				'helo_identity' => $sessionData->{'Helo'}, # optional,
 		);
 
-        # Eval to catch sig ALRM
+		# Eval to catch sig ALRM
 		my $result;
-        eval {
+		eval {
 			local $SIG{'ALRM'} = sub { die "Timed Out!\n" };
 
 			# Give query 15s to return
-           	alarm(15);
+			alarm(15);
 
 			# Get result
 			$result = $spf_server->process($rqst);
-        };
+		};
 
 		# Check results
 		if ($@ =~ /timed out/i) {
